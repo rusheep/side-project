b9c9201f43021f208a05f0ec9e1ccad29d4c373b
rusheep <yo0983987618@gmail.com>
2023-03-16 09:02:37 +0800

    背景效果

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -53,8 +53,8 @@ $(document).ready(function() {
     $('.btn').mouseover(function() {
       var originalHeight = $('.bg-change').height();
       $('.bg-change').css({
-        'width': originalHeight +120* 20,
-        'height': originalHeight +120* 20
+        'width': originalHeight +220* 20,
+        'height': originalHeight +220* 20
       });
     }).mouseout(function() {
       $('.bg-change').css({
