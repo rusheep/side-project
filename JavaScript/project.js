23e931e2d6073a379621e85934462c6b1f5a6366
rusheep <yo0983987618@gmail.com>
2023-03-25 08:53:05 +0800

    資料串接

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -1,42 +1,51 @@
-var myObject = [ {id:1, type:'人物' },
-    {id:2, type:'場景' },
-    {id:3, type:'場景' },
-    {id:4, type:'立繪' },
-    {id:5, type:'人物' },
-    {id:6, type:'人物' },
-    {id:7, type:'人物' },
-    {id:8, type:'頭貼' },
-    {id:9, type:'人物' },
-    {id:10, type:'場景' },
-    {id:11, type:'人物' },
-    {id:12, type:'人物' },
-    {id:13, type:'人物' },
-    {id:14, type:'頭貼' },
-    {id:15, type:'人物' },
-    {id:16, type:'人物' },
-    {id:17, type:'頭貼' },
-    {id:18, type:'場景' },
-    {id:19, type:'人物' },
-    {id:20, type:'人物' },
-    {id:21, type:'人物' },
-    {id:22, type:'人物' },
-    {id:23, type:'人物' },
-    {id:24, type:'立繪' },
-    {id:25, type:'頭貼' },
-    {id:26, type:'立繪' },
+var myObject = [ {id:'01', type:'人物' },
+    {id:'02', type:'場景' },
+    {id:'03', type:'場景' },
+    {id:'04', type:'立繪' },
+    {id:'05', type:'人物' },
+    {id:'06', type:'人物' },
+    {id:'07', type:'人物' },
+    {id:'08', type:'頭貼' },
+    {id:'09', type:'人物' },
+    {id:'10', type:'場景' },
+    {id:'11', type:'人物' },
+    {id:'12', type:'人物' },
+    {id:'13', type:'人物' },
+    {id:'14', type:'頭貼' },
+    {id:'15', type:'人物' },
+    {id:'16', type:'人物' },
+    {id:'17', type:'頭貼' },
+    {id:'18', type:'場景' },
+    {id:'19', type:'人物' },
+    {id:'20', type:'人物' },
+    {id:'21', type:'人物' },
+    {id:'22', type:'人物' },
+    {id:'23', type:'人物' },
+    {id:'24', type:'立繪' },
+    {id:'25', type:'頭貼' },
+    {id:'26', type:'立繪' },
 ];
 
-// for (let i = 0; i < myObject.length; i++) {
-//     const imageId = `image-${myObject[i].id}`;
-//     const imageUrl = `project-${myObject[i].id}.jpg`;
-//     // document.getElementById(imageId).src = imageUrl;
-//     const img = document.createElement('img');
-//     img.setAttribute('id', imageId);
-//     img.setAttribute('src', imageUrl);
-    
-//     // 找到對應的 div 元素並添加 img 元素
-//     const div = document.querySelector(`.pj-item:nth-of-type(${i+1})`);
-//     div.appendChild(img);
-// }
+// console.log(myObject[0].id);
 
-// console.log (myObject);
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    for (let i = 0; i < myObject.length; i++) {
+        const imageId = `image-${myObject[i].id}`;
+        const imageUrl = `project${myObject[i].id}.jpg`;
+        let pj_content_el = document.querySelector(".pj-content");
+        console.log(pj_content_el);
+        let pic_str =
+        `<div class="pj-item">
+            <img id="image-1${imageId}" src="../img/project_img/${imageUrl}" alt="">
+        </div>`
+        // console.log(imageId);
+        // console.log(imageUrl);
+        pj_content_el.insertAdjacentHTML("beforeend", pic_str);
+        // console.log(pic_str);
+        
+    }
+    
+    // console.log (myObject);
+});
