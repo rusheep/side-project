b28647ce02a82a4fa282aadd1d483e053f09bce7
rusheep <yo0983987618@gmail.com>
2023-03-30 01:01:39 +0800

    btn-filter80%

--- JavaScript/project.js
+++ JavaScript/project.js
@@ -1,51 +1,107 @@
-var myObject = [ {id:'01', type:'人物' },
-    {id:'02', type:'場景' },
-    {id:'03', type:'場景' },
-    {id:'04', type:'立繪' },
-    {id:'05', type:'人物' },
-    {id:'06', type:'人物' },
-    {id:'07', type:'人物' },
-    {id:'08', type:'頭貼' },
-    {id:'09', type:'人物' },
-    {id:'10', type:'場景' },
-    {id:'11', type:'人物' },
-    {id:'12', type:'人物' },
-    {id:'13', type:'人物' },
-    {id:'14', type:'頭貼' },
-    {id:'15', type:'人物' },
-    {id:'16', type:'人物' },
-    {id:'17', type:'頭貼' },
-    {id:'18', type:'場景' },
-    {id:'19', type:'人物' },
-    {id:'20', type:'人物' },
-    {id:'21', type:'人物' },
-    {id:'22', type:'人物' },
-    {id:'23', type:'人物' },
-    {id:'24', type:'立繪' },
-    {id:'25', type:'頭貼' },
-    {id:'26', type:'立繪' },
+let myObject = [{ id: '01', type: '人物' },
+{ id: '02', type: '場景' },
+{ id: '03', type: '場景' },
+{ id: '04', type: '立繪' },
+{ id: '05', type: '人物' },
+{ id: '06', type: '人物' },
+{ id: '07', type: '人物' },
+{ id: '08', type: '頭貼' },
+{ id: '09', type: '人物' },
+{ id: '10', type: '場景' },
+{ id: '11', type: '人物' },
+{ id: '12', type: '人物' },
+{ id: '13', type: '人物' },
+{ id: '14', type: '頭貼' },
+{ id: '15', type: '人物' },
+{ id: '16', type: '人物' },
+{ id: '17', type: '頭貼' },
+{ id: '18', type: '場景' },
+{ id: '19', type: '人物' },
+{ id: '20', type: '人物' },
+{ id: '21', type: '人物' },
+{ id: '22', type: '立繪' },
+{ id: '23', type: '人物' },
+{ id: '24', type: '立繪' },
+{ id: '25', type: '頭貼' },
 ];
 
 // console.log(myObject[0].id);
 
 
-document.addEventListener('DOMContentLoaded', function(){
 
-    for (let i = 0; i < myObject.length; i++) {
+
+document.addEventListener('DOMContentLoaded', displayImg);
+let pj_content_el = document.querySelector(".pj-content");
+
+
+function displayImg() {
+    //output
+
+    pj_content_el.innerHTML = '';
+
+    myObject.forEach((item,i) => {
         const imageId = `image-${myObject[i].id}`;
         const imageUrl = `project${myObject[i].id}.jpg`;
-        let pj_content_el = document.querySelector(".pj-content");
-        console.log(pj_content_el);
+
+
         let pic_str =
-        `<div class="pj-item">
+            `<div class="pj-item">
             <img id="image-1${imageId}" src="../img/project_img/${imageUrl}" alt="">
         </div>`
-        // console.log(imageId);
-        // console.log(imageUrl);
-        pj_content_el.insertAdjacentHTML("beforeend", pic_str);
-        // console.log(pic_str);
+
+        pj_content_el.insertAdjacentHTML("beforeend", pic_str); 
+    })
+};
+
+
+
+function btn_filter(btn,img_type) {
+    let btn_switch =false;
+    btn.addEventListener('click',function(e) {
+
+
+        // const all_btn = document.querySelectorAll(".btn");
+        // console.log(all_btn[0]);
+        // all_btn.forEach((item,i)=> {
+        //     all_btn[i].classList.remove('btn-on');
+        // })
+        // btn.style.classList.add('btn-on');
+
+
+        btn_switch = !btn_switch;
+        e.preventDefault();
+        pj_content_el.innerHTML = '';
         
-    }
-    
-    // console.log (myObject);
-});
+        if(btn_switch) {
+
+            const peopleArr = myObject.filter(item => item.type == img_type);
+
+            peopleArr.forEach((item, i) => {
+                // console.log(item);
+                let pic_str =
+                    `<div class="pj-item">
+                <img id="image-1${item.id}" src="../img/project_img/project${item.id}.jpg" alt="">
+            </div>`
+
+                pj_content_el.insertAdjacentHTML("beforeend", pic_str);
+            })
+        }else {
+            displayImg();
+        }
+    });
+};
+
+
+const btn1 = document.querySelector('.btn1');
+btn_filter(btn1,"人物");
+
+const btn2 = document.querySelector('.btn2');
+btn_filter(btn2,"場景");
+
+
+const btn3 = document.querySelector('.btn3');
+btn_filter(btn3,"立繪");
+
+
+const btn4 = document.querySelector('.btn4');
+btn_filter(btn4,"頭貼");
