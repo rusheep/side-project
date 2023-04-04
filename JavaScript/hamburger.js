36ffbf8b4a1079ca89eabd85c89d968914ff45ef
rusheep <yo0983987618@gmail.com>
2023-04-04 21:55:00 +0800

    home rwd 50%

--- JavaScript/hamburger.js
+++ JavaScript/hamburger.js
@@ -3,10 +3,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburger = document.querySelector(".hamburger");
     const navLinks = document.querySelector(".navbar");
     const links = document.querySelectorAll(".navbar li");
+    const logo = document.querySelector(".logo");
 
     hamburger.addEventListener('click', ()=>{
     //Animate Links
         navLinks.classList.toggle("open");
+        logo.classList.toggle('logo-on');
         links.forEach(link => {
             link.classList.toggle("fade");
         });
