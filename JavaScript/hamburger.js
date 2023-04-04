4b55f6408bd165d457839e91617e545576e2fcc2
rusheep <yo0983987618@gmail.com>
2023-04-04 13:05:28 +0800

    index rwd 5%

---
+++ JavaScript/hamburger.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".navbar");
+    const links = document.querySelectorAll(".navbar li");
+
+    hamburger.addEventListener('click', ()=>{
+    //Animate Links
+        navLinks.classList.toggle("open");
+        links.forEach(link => {
+            link.classList.toggle("fade");
+        });
+
+        //Hamburger Animation
+        hamburger.classList.toggle("toggle");
+    });
+
+});
